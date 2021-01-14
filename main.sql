e6befb37c2ca491f87321796191d44dc29645b41
Grace Hudgens <91076277@g.mvusd.net>
2021-01-14 01:10:58 +0000

    liked companies/internships w/ primary/foreign keys

--- main.sql
+++ main.sql
@@ -4,7 +4,7 @@
 /*A table for the companies, to include the Name, location, business sector and contact information*/
 CREATE TABLE companies
 (
-  company_name TEXT,
+  company_name TEXT PRIMARY KEY,
   location ADDRESS,
   sector TEXT,
   contact_info INTEGER
@@ -13,22 +13,24 @@ CREATE TABLE companies
 /*A table for the internship positions, to include the position title, company name, application deadline, duration of the position, description of responsibilities and requirements, pay and any other pertinent information.*/
 CREATE TABLE internships
 (
-  position TEXT,
+  position TEXT PRIMARY KEY,
   company_name TEXT,
   deadline TEXT,
   duration_days INTEGER,
   describe TEXT,
-  pay DOUBLE
+  pay DOUBLE,
+  FOREIGN KEY (company_name) REFERENCES companies(company_name)
 );
 
 INSERT INTO companies VALUES
 ('test', 'test', 'test', 4322221);
 
-.print 'companies'
+.print 'Companies'
 SELECT * FROM companies;
 
 INSERT INTO internships VALUES
 ('test', 'test', 'test', 42, 'test', 2.33);
 
-.print 'internships'
+.print
+.print 'Internships'
 SELECT * FROM internships;
