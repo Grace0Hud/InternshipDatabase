1e24a4265a62bd63e18cd422136f62abad2cfba7
Grace Hudgens <91076277@g.mvusd.net>
2021-01-14 00:26:39 +0000

    created companies table (test values)

--- main.sql
+++ main.sql
@@ -1,2 +1,16 @@
 .header on
 .mode column
+
+CREATE TABLE companies
+(
+  company_name TEXT,
+  location ADDRESS,
+  sector TEXT,
+  contact_info INTEGER
+);
+
+INSERT INTO companies VALUES
+('test', 'test', 'test', 4322221);
+
+.print 'companies'
+SELECT * FROM companies
