56b664fb662652e6f5fd971c9697b2420afd2e75
Grace Hudgens <91076277@g.mvusd.net>
2021-01-21 17:19:48 +0000

    all internships found

--- main.sql
+++ main.sql
@@ -23,12 +23,16 @@ CREATE TABLE internships
 );
 
 INSERT INTO companies VALUES
-('SoCal Edison', 'Rosemead California', 'Power', '18006554555'),
+('SoCal Edison', 'Rosemead California', 'Power', '1(800)655-4555'),
 ('Dropbox', 'San Fransisco, CA', 'remote collaboration', 'https://www.dropbox.com/support'),
 ('American Express', 'Remote', 'banking', 'Recruitment.Support.Americas@aexp.com'),
 ('Fivestars', 'San Francisco, CA', 'customer loyalty programs', 'NA'),
 ('VMWare', 'Palo Alto, CA', 'Virtual Machines', 'careers@vmware.com'),
-('Amazon', 'Moreno Valley, CA', 'shipping', 'NA');
+('Amazon', 'Moreno Valley, CA', 'shipping', 'NA'),
+('Serco', 'USA', 'public service', '(703)263-6000'),
+('Bae Systems', 'Hill AFB, Utah', 'private defense','1(571)461-6000'),
+('Tyler Technologies', 'remote', 'education technology', '(972)713-3700'),
+('TCG', 'Remote', 'investment', 'info@tcg.co');
 
 .print 'COMPANIES'
 SELECT * FROM companies;
@@ -40,7 +44,11 @@ INSERT INTO internships VALUES
 ('Campus Recruitment - Technology', 'American Express', '2021-06', 70, 'https://jobs.americanexpress.com/global/jobs/20006515?lang=en-us&src=JB-10780&utm_source=Indeed&utm_medium=organic&utm_campaign=Indeed', 0.00),
 ('Software Engineer Intern', 'Fivestars', '2021-06', 91, 'https://www.fivestars.com/careers/?gh_jid=2324334', 404),
 ('Core Graphics Technical Team', 'VMWare', 'soon?', 0, 'https://careers.vmware.com/main/jobs/R2011142?lang=en-us&source=LIPJ', 0),
-('IT Support Engineer', 'Amazon', '2021-06', 70, 'https://www.glassdoor.com/partner/jobListing.htm?pos=102&ao=133043&s=58&guid=000001772349aaab9b37a77ffa964f77&src=GD_JOB_VIEW&t=SR&vt=w&cs=1_21f52a37&cb=1611204767081&jobListingId=3771134124', 404);
+('IT Support Engineer', 'Amazon', '2021-06', 70, 'https://www.glassdoor.com/partner/jobListing.htm?pos=102&ao=133043&s=58&guid=000001772349aaab9b37a77ffa964f77&src=GD_JOB_VIEW&t=SR&vt=w&cs=1_21f52a37&cb=1611204767081&jobListingId=3771134124', 404),
+('STEP Tecnical track', 'Serco', '2021-05-24', 81, 'https://careers-sercona.icims.com/jobs/37457/2021-step-summer-internship-program-%28technical-track%29/job', 404),
+('ICBM Cybersecurity', 'Bae Systems', '2021', 0, 'https://jobs.baesystems.com/global/en/job/63371BR/2021-ICBM-Cybersecurity-Intern-Co-op-Computer-Science-Computer-Engineering-Information-Technology', 404),
+('Graphic Design Intern', 'Tyler Technologies', '2021-04', 0,'https://www.dice.com/jobs/detail/f1b3e345cbb3cbc69fea6f9f943eb539?src=32&utm_source=appcast&utm_medium=aggregator&utm_campaign=talent', 0),
+('Engineering Intern', 'TCG', '2021-06-07', 67, 'https://boards.greenhouse.io/tcgcapital/jobs/5033454002?gh_src=8121a8002us', 404);
 
 .print '------------------------------------------------'
 .print
